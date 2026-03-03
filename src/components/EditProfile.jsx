ec57df933c69305e7daa999b12efc91dee234396
Akshay Gaur <akshay.gaur028@gmail.com>
2026-03-03 22:34:39 +0530

    ./UserCard path correction in EditProfile.jsx

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 import { BASE_URL } from "../utils/constant";
 import axios from "axios";
 import { useDispatch } from "react-redux";
